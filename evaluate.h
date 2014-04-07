f766f51f9450c7636535e0526b6ec1e52cff8f50
destruir <hichoshi@gmail.com>
2014-04-07 14:55:47 -0400

    TEST

---
+++ evaluate.h
@@ -0,0 +1,8 @@
+#ifndef _evaluate_h
+#define _evaluate_h
+
+void card_evaluator();
+
+void compare_final_hands();
+
+#endif
---
+++ winnings.h
@@ -0,0 +1,8 @@
+#ifndef _winners_h
+#define _winners_h
+
+void distribute_winnings();
+
+void keep_track_of_winnings();
+
+#endif
