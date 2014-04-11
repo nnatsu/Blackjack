f73a41986de194fb4a619c675605639b29a0241c
egbk2000 <egbk2000@users.noreply.github.com>
2014-04-11 19:10:23 -0400

    Update main.h

--- main.h
+++ main.h
@@ -2,14 +2,16 @@
 #define MAX_CARDS 15
 #define INITIAL_MONEY 1000
 #define MIN_BET 5
-char handComp[MAX_CARDS];
+#define NUM_SUITS 4
+#define NUM_RANKS 13
+char handComp[MAX_CARDS][2];
 int numRounds;              //the number of rounds that have been played so far
 int numPlayers;             //the number of human players playing
 int playing;                //variable to hold whether or not to keep playing the game, 1=yes 0=no
 struct player {
     int bet;                //the bet for the current round
     int currentM;           //the amount of money the player currently has
-    char hand[MAX_CARDS];   //the hands the player currently has
+    char hand[MAX_CARDS][2];   //the hands the player currently has
 };
 
 struct player players[MAX_PLAYERS];     //array of players in game
