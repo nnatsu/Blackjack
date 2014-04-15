e8b8e7c61386ada36ab1d27ab0c8abdec35b16ea
amastriann <amastrianni12@gmail.com>
2014-04-14 23:30:19 -0400

    Added levels variable to main

--- main.h
+++ main.h
@@ -8,6 +8,7 @@ char handComp[MAX_CARDS][2];
 int numRounds;              //the number of rounds that have been played so far
 int numPlayers;             //the number of human players playing
 int playing;                //variable to hold whether or not to keep playing the game, 1=yes 0=no
+int levels;
 struct player {
     int bet;                //the bet for the current round
     int currentM;           //the amount of money the player currently has
