bcb55a61e15a4c228ac024c50228689cfc0a7a0f
amastriann <amastrianni12@gmail.com>
2014-04-14 23:28:47 -0400

    Updated rounds file

---
+++ rounds.c
@@ -0,0 +1,30 @@
+
+#include<main.h>
+#include<rounds.h>
+
+/*
+int playAgain: Asks the users if they would like to play again. Returns 1 if they answer yes, returns 0 if they answer no.
+*/ 
+int playAgain() {
+  printf("Do you want to play again? (Answer y or n)\n");
+  char c;
+  int x = 0;
+  scanf("%c", &c);
+  //look for correct input
+  while(x == 0) {
+    if(c != 'y' && c != 'n') {
+      printf("I am sorry but your input was not correct. Would you like to play again\n");
+      scanf("%c", &c);
+    } else {
+      x = 1;
+    }
+  }
+  //return yes or no
+  if(c == 'y') {
+    printf("y\n");
+    return 1;
+  } else  {
+    printf("n\n");
+    return 0;
+  }
+}
