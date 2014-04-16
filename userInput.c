77519e0b1c502c73c968621ceced01cafd529c85
egbk2000 <egbk2000@users.noreply.github.com>
2014-04-16 14:42:07 -0400

    Update userInput.c

--- userInput.c
+++ userInput.c
@@ -4,6 +4,7 @@
 #include "main.h"
 #include "userInput.h"
 
+int printed;
 
 /***asks and stores how many human players will play in current round***/
 void numberOfPlayers(){
@@ -34,6 +35,8 @@ void numberOfPlayers(){
 void betTaker(){
     for (int i = 0; i < numPlayers; i++){
         //ask user to input their bet
+        printed = 0;
+        
         printf("Player %d, please enter your bet (minimum bet is $%d): ", i+1, MIN_BET);
         scanf("%d", &players[i].bet);
         
@@ -47,20 +50,31 @@ void betTaker(){
 }
 
 int validBet(int b, int p){
+    
     //if bet is greater than their currentM ask for new bet
     if (b > players[p].currentM){
-        printf("Your bet exceeds the amount of money you currently have. You currently have $%d. Please enter a new bet: ", players[p].currentM);
-        scanf("%d", &players[p].bet);
-        printf("Bet was: %d\n", players[p].bet);
+        if (printed == 0){
+            printf("Your bet exceeds the amount of money you currently have. You currently have $%d. Please enter a new bet: ", players[p].currentM);
+            printed++;
+        } else {
+            scanf("%d", &players[p].bet);
+        }
+        //printf("printed was: %d\n", printed);
         getchar();
+        
         return 1;
     }
     
     //if bet is less than min_bet ask for new bet
     if (b < MIN_BET){
-        printf("Your bet is less than the minimum bet. Please enter a new bet: ");
-        scanf("%d", &players[p].bet);
-        printf("Bet was: %d\n", players[p].bet);
+        if (printed == 0){
+            printf("Your bet is less than the minimum bet. Please enter a new bet: ");
+            scanf("%d", &players[p].bet);
+            printed++;
+        } else {
+            scanf("%d", &players[p].bet);
+        }
+        //printf("printed was: %d\n", printed);
         getchar();
         return 1;
     }
