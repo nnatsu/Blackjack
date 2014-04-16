f424bfd3ea6773aeea1545659150f6b25d88fadc
amastriann <amastrianni12@gmail.com>
2014-04-16 00:53:27 -0400

    changes made to rounds and main

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ int numRounds;              //the number of rounds that have been played so far
 int numPlayers;             //the number of human players playing
 int playing;                //variable to hold whether or not to keep playing the game, 1=yes 0=no
 int levels; //the level chosen by the player
-char dealt[MAX_CARDS];
+char dealt[MAX_CARDS][2];
 struct player {
     int bet;                //the bet for the current round
     int currentM;           //the amount of money the player currently has
