a66868d21db414d4c069186d5a1e7860d564957d
egbk2000 <egbk2000@users.noreply.github.com>
2014-04-19 11:15:22 -0400

    Update main.h

--- main.h
+++ main.h
@@ -9,13 +9,13 @@ int compCards;
 int numRounds;              //the number of rounds that have been played so far
 int numPlayers;             //the number of human players playing
 int playing;                //variable to hold whether or not to keep playing the game, 1=yes 0=no
-int levels; //the level chosen by the player
+int levels;                 //the level chosen by the player
 char dealt[MAX_CARDS][2];
 struct player {
-    int bet;                //the bet for the current round
-    int currentM;           //the amount of money the player currently has
-    char hand[MAX_CARDS][2]; //the hands the player currently has
-  int numCards; //the number of cards that they player has been dealt
+    int bet;                    //the bet for the current round
+    int currentM;               //the amount of money the player currently has
+    char hand[MAX_CARDS][2];    //the hands the player currently has
+    int numCards;               //the number of cards that they player has been dealt
 };
 
 struct player players[MAX_PLAYERS];     //array of players in game
