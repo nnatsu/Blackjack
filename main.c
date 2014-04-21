d254e555b5e6030ff24d3ace259b5c842ee57d1a
egbk2000 <egbk2000@users.noreply.github.com>
2014-04-21 13:58:22 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+#include "userInput.h"
+#include "dealing.h"
+#include "hit.h"
+#include "rounds.h"
+#include "evaluate.h"
+#include "winnings.h"
+
+int main(){
+    //ask for number of players
+    numberOfPlayers();
+    //ask players to choose a level
+    selectLevel();
+    //take each players bet
+    betTaker();
+    //deal all players their  intial hand
+    dealHand();
+    //display each players hand
+    displayHand();
+    //loop through each player
+    for (int i = 0; i <numPlayers; i++){
+        printf("Player %d:\n", i+1);
+        int hit = hitDecide();
+        while (hit == 1) {
+            dealCard(i, 0);
+            displayHand();
+            hit = hitDecide();
+        }
+    }
+    return 0;
+}
