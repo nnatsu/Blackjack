e9504e4d12a3b5690c03986d5f3f39dd1f3df6ad
egbk2000 <egbk2000@users.noreply.github.com>
2014-04-27 11:02:57 -0400

    added situation for draw

--- winnings1.c
+++ winnings1.c
@@ -6,10 +6,14 @@ int distributeWinnings(int winner, int currentPlayer){
     if (winner == 1){
         printf("Player %d wins $%d\n", currentPlayer+1, (players[currentPlayer].bet)*3);
         return (players[currentPlayer].bet)*3;
-    } else {
+    } else if (winner == 0){
         printf("Player %d lost $%d\n", currentPlayer+1, -(players[currentPlayer].bet)*2);
         return -(players[currentPlayer].bet)*2;
+    } else if (winner == 2){
+        printf("There was a draw, both the dealer and player %d had the same number of points. Player %d gets to keep their bet of %d\n", currentPlayer+1, currentPlayer+1, players[currentPlayer].bet);
+        return 0;
     }
+    return 0;
 }
 
 /***Takes in the amount won or lost by a player and modifies how much money they currently have***/
