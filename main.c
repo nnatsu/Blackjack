77584856da47732906a1ac78aeb32457ab704d2a
egbk2000 <egbk2000@users.noreply.github.com>
2014-04-27 12:00:52 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
 #include "main.h"
 #include "userInput.h"
 #include "dealing.h"
 #include "hit.h"
 #include "rounds.h"
-#include "evaluate.h"
-#include "winnings.h"
+#include "evaluate1.h"
+#include "winnings1.h"
+//#include "evaluate.h"
+//#include "winnings.h"
 
 int main(){
     //CONNIE init stuff used in evaluate.h
-    init_stat();
+    //init_stat();
+    
     //ask for number of players
     numberOfPlayers();
     //ask players to choose a level
@@ -24,16 +24,22 @@ int main(){
     //display each players hand
     displayHand();
     //loop through each player
-    for (int i = 0; i <numPlayers; i++){
+    for (int i = 0; i < numPlayers; i++){
         printf("Player %d:\n", i+1);
-        int hit = hitDecide();
+        int hit = hitDecide(i);
         while (hit == 1) {
             dealCard(i, 0);
             displayHand();
-            hit = hitDecide();
+            hit = hitDecide(i);
         }
+    }
+    //hit for the computer
+    computerHit();
+    
+    for (int i = 0; i < numPlayers; i++){
+        int winner = evaluate(i);
+        int money = distributeWinnings(winner, i);
+        keepTrackOfWinnings(money, i);
     }
     return 0;
 }
-
-#endif
