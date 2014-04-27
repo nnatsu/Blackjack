a48f7518801130e4dd1e311168c53e17da97df7e
egbk2000 <egbk2000@users.noreply.github.com>
2014-04-27 12:01:15 -0400

    Update evaluate1.c

--- evaluate1.c
+++ evaluate1.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-#include "evaluate.h"
+#include "evaluate1.h"
 #include "main.h"
-#include "winnings.h"
+#include "winnings1.h"
 #include "hit.h"
 
 int evaluate(int j) {
+
   int computerVal = totalValue(100);
   int playerVal = totalValue(j);
-  printf("The total of the computer's final hand is %d and the total of the player's final hand is %d\n", computerVal, playerVal);
+  printf("The total of the computer's final hand is %d and the total of the player %d's final hand is %d\n", computerVal, j+1, playerVal);
   if (computerVal > 21 && playerVal > 21) {
     return 2;
-  } else if (computerVal > playerVal) {
+  } else if (playerVal > 21){
+      return 0;
+  }else if (computerVal > playerVal) {
     return 0;
   } else if (playerVal > computerVal) {
     return 1;
